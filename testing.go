660025599a686dc9ac2a058169a74bc144006e37
Beni Pranata <beni@pewara.com>
2026-01-03 18:06:11 +0700

    testing: add comments

--- testing.go
+++ testing.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Require asserts that actual matches expected error.
 func Require(t testing.TB, expected error, actual error, msgAndArgs ...any) {
 	if expected == nil {
 		require.NoError(t, actual, msgAndArgs...)
@@ -15,10 +16,12 @@ func Require(t testing.TB, expected error, actual error, msgAndArgs ...any) {
 	}
 }
 
+// RequireNil asserts that err is nil.
 func RequireNil(t testing.TB, err error, msgAndArgs ...any) {
 	require.NoError(t, err, msgAndArgs...)
 }
 
+// RequireNotNil asserts that err is not nil.
 func RequireNotNil(t testing.TB, err error, msgAndArgs ...any) {
 	require.Error(t, err, msgAndArgs...)
 }
